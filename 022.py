75622461f2eb7b1d67469699799fb34133f5aad5
Yoshigon <yoshihiro.nakayama.j@gmail.com>
2022-08-16 15:56:52 +0900

    2022/08/16 speed improved

--- 022.py
+++ 022.py
@@ -1,4 +1,6 @@
 import math
+
 A, B, C = map(int, input().split())
-gcd = math.gcd(A, math.gcd(B, C))
-print(A // gcd + B // gcd + C // gcd - 3)
+gcd = math.gcd(A, (math.gcd(B, C)))
+
+print((A+B+C)//gcd - 3)
