2a851dce25329786b53a75fbf018d6781015f558
Yoshigon <yoshihiro.nakayama.j@gmail.com>
2022-08-17 18:48:26 +0900

    2022/08/17 improved speed

--- 021.py
+++ 021.py
@@ -1,10 +1,11 @@
+from collections import defaultdict
 import sys
-sys.setrecursionlimit(10 ** 6)
-
-
+sys.setrecursionlimit(10**6)
 # 強連結成分分解(SCC): グラフGに対するSCCを行う
 # 入力: <n>: 頂点サイズ, <g>: 順方向の有向グラフ, <rg>: 逆方向の有向グラフ
 # 出力: (<ラベル数>, <各頂点のラベル番号>)
+
+
 def scc(n, g, rg):
     order = []
     used = [0] * n
@@ -35,36 +36,23 @@ def scc(n, g, rg):
     return label, group
 
 
-# 縮約後のグラフを構築
-def construct(n, g, label, group):
-    g0 = [set() for i in range(label)]
-    gp = [[] for i in range(label)]
-    for v in range(n):
-        lbs = group[v]
-        for w in g[v]:
-            lbt = group[w]
-            if lbs == lbt:
-                continue
-            g0[lbs].add(lbt)
-        gp[lbs].append(v)
-    return g0, gp
-
-
 N, M = map(int, input().split())
-edge = [[] for _ in range(N)]
-reversed_edge = [[] for _ in range(N)]
+G = [[] for _ in range(N)]
+RG = [[] for _ in range(N)]
+
 for _ in range(M):
     a, b = map(int, input().split())
-    a -= 1
-    b -= 1
-    edge[a].append(b)
-    reversed_edge[b].append(a)
+    G[a-1].append(b-1)
+    RG[b-1].append(a-1)
 
-label, group = scc(N, edge, reversed_edge)
-G0, GP = construct(N, edge, label, group)
-
+num_groups, group_labels = scc(N, G, RG)
 ans = 0
-for g in GP:
-    ans += len(g) * (len(g)-1) // 2
+group_members = defaultdict(lambda: 0)
+
+for i in range(N):
+    group_members[group_labels[i]] += 1
+
+for val in group_members.values():
+    ans += val * (val-1) // 2
 
 print(ans)
