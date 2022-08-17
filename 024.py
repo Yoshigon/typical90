9c6d85bdca626720efcee9ab63d7e601ac0addf6
Yoshigon <yoshihiro.nakayama.j@gmail.com>
2022-08-17 18:29:26 +0900

    2022/08/17 minor change

--- 024.py
+++ 024.py
@@ -2,14 +2,13 @@ N, K = map(int, input().split())
 A = list(map(int, input().split()))
 B = list(map(int, input().split()))
 
-count = 0
-
+dif = 0
 for i in range(N):
-    count += abs(A[i]-B[i])
+    dif += abs(A[i]-B[i])
 
-if count > K:
+if dif > K:
     print('No')
-elif (count-K) % 2 == 0:
-    print('Yes')
+elif (dif - K) % 2 != 0:
+    print('No')
 else:
-    print('No')
+    print('Yes')
