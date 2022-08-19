46e76d979ee6e477c9b0e2b97ba903bab8257985
Yoshigon <yoshihiro.nakayama.j@gmail.com>
2022-08-19 09:07:26 +0900

    2022/08/19

--- 028.py
+++ 028.py
@@ -1,25 +1,25 @@
 N = int(input())
-grid = [[0] * 1002 for _ in range(1002)]
+grid = [[0] * 1001 for _ in range(1001)]
+area = {key: 0 for key in range(N+1)}
+
 for _ in range(N):
     lx, ly, rx, ry = map(int, input().split())
+    grid[lx][ly] += 1
+    grid[lx][ry] -= 1
+    grid[rx][ly] -= 1
+    grid[rx][ry] += 1
 
-    grid[ly][lx] += 1
-    grid[ly][rx] -= 1
-    grid[ry][lx] -= 1
-    grid[ry][rx] += 1
+for i in range(1001):
+    for j in range(1000):
+        grid[i][j+1] += grid[i][j]
 
-for i in range(1002):
-    for j in range(1, 1002):
-        grid[i][j] += grid[i][j-1]
+for i in range(1000):
+    for j in range(1001):
+        grid[i+1][j] += grid[i][j]
 
-for j in range(1002):
-    for i in range(1, 1002):
-        grid[i][j] += grid[i-1][j]
-
-dic = {key: 0 for key in range(N+1)}
-for i in range(1002):
-    for j in range(1002):
-        dic[grid[i][j]] += 1
+for i in range(1001):
+    for j in range(1001):
+        area[grid[i][j]] += 1
 
 for k in range(1, N+1):
-    print(dic[k])
+    print(area[k])
