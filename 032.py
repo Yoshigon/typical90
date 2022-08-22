a476379981907173ee4a1f0b69385d8fdcaff1b2
Yoshigon <yoshihiro.nakayama.j@gmail.com>
2022-08-22 17:24:34 +0900

    2022/08/22 improved speed

--- 032.py
+++ 032.py
@@ -1,32 +1,32 @@
 from itertools import permutations
-
 N = int(input())
 time = [list(map(int, input().split())) for _ in range(N)]
 M = int(input())
-bad_relation = {key: {} for key in range(N)}
+bad_relations = [set() for _ in range(N)]
 for _ in range(M):
     x, y = map(int, input().split())
     x -= 1
     y -= 1
-    bad_relation[x][y] = 1
-    bad_relation[y][x] = 1
-ans = 10 ** 5
+    bad_relations[x].add(y)
+    bad_relations[y].add(x)
 
-for v in permutations(range(N)):
+ans = 10001
+
+for p in permutations(range(N)):
     flag = 0
+    tmp = 0
     for i in range(N-1):
-        if v[i+1] in bad_relation[v[i]]:
+        if p[i] in bad_relations[p[i+1]]:
             flag = 1
             break
     if flag:
         continue
+    else:
+        for i in range(N):
+            tmp += time[p[i]][i]
+        ans = min(ans, tmp)
 
-    tmp = 0
-    for i in range(N):
-        tmp += time[v[i]][i]
-    ans = min(ans, tmp)
-
-if ans != 10 ** 5:
-    print(ans)
-else:
+if ans == 10001:
     print(-1)
+else:
+    print(ans)
