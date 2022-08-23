431baea3771b7200f0a6f5065d035f7fc5256002
Yoshigon <yoshihiro.nakayama.j@gmail.com>
2022-08-23 14:10:37 +0900

    2022/08/23 improved length

--- 034.py
+++ 034.py
@@ -1,32 +1,27 @@
 N, K = map(int, input().split())
 A = list(map(int, input().split()))
 
-num_types = 1
-num_dic = {A[0]: 1}
-ans = 1
+num_count = {A[0]: 1}
 head = 0
 tail = 1
+types = 1
+ans = 1
 
 while tail < N:
-    if num_types > K:
-        num_dic[A[head]] -= 1
-        if num_dic[A[head]] == 0:
-            num_types -= 1
-        head += 1
+    tail += 1
+    if A[tail-1] not in num_count:
+        num_count[A[tail-1]] = 1
+        types += 1
+    elif num_count[A[tail-1]] == 0:
+        num_count[A[tail-1]] = 1
+        types += 1
     else:
-        ans = max(ans, tail-head)
-        tail += 1
-        if A[tail-1] in num_dic:
-            if num_dic[A[tail-1]] != 0:
-                num_dic[A[tail-1]] += 1
-            else:
-                num_dic[A[tail-1]] = 1
-                num_types += 1
-        else:
-            num_dic[A[tail-1]] = 1
-            num_types += 1
-
-if num_types <= K:
+        num_count[A[tail-1]] += 1
+    while types > K:
+        num_count[A[head]] -= 1
+        if num_count[A[head]] == 0:
+            types -= 1
+        head += 1
     ans = max(ans, tail-head)
 
 print(ans)
