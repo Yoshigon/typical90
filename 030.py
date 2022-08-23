34d83c9ad13465a8abddd48b401ce4cf9b68f39c
Yoshigon <yoshihiro.nakayama.j@gmail.com>
2022-08-23 13:36:51 +0900

    2022/08/23 improved speed, memory, length

--- 030.py
+++ 030.py
@@ -1,20 +1,12 @@
 N, K = map(int, input().split())
-
-num_with_k_prime_factors = {key: 0 for key in range(9)}
-num_prime_factors = [0 for i in range(N+1)]  # 1からNの数がもつ素因数の数
+num_p_factors = [0] * (10**7+1)
+ans = 0
 
 for i in range(2, N+1):
-    if num_prime_factors[i] == 0:  # 素数の場合
+    if num_p_factors[i] == 0:
         for j in range(i, N+1, i):
-            num_prime_factors[j] += 1
-
-    if num_prime_factors[i] > 8:
-        num_with_k_prime_factors[8] += 1
-    else:
-        num_with_k_prime_factors[num_prime_factors[i]] += 1
-
-ans = 0
-for k in range(K, 9):
-    ans += num_with_k_prime_factors[k]
+            num_p_factors[j] += 1
+            if num_p_factors[j] == K:
+                ans += 1
 
 print(ans)
