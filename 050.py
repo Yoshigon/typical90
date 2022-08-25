4c91189d32f64b34cc0293a0f9c6d5350fa2d503
Yoshigon <yoshihiro.nakayama.j@gmail.com>
2022-08-25 23:45:24 +0900

    2022/08/25 improved speed, length

--- 050.py
+++ 050.py
@@ -1,14 +1,11 @@
 N, L = map(int, input().split())
+dp = [1] + [0] * N
 MOD = 10 ** 9 + 7
-dp = [0] * (N+1)
-dp[0] = 1
 
 for i in range(N):
+    dp[i] %= MOD
     dp[i+1] += dp[i]
-    dp[i+1] %= MOD
-
-    if i+L <= N:
+    if i+L < N+1:
         dp[i+L] += dp[i]
-        dp[i+L] %= MOD
 
-print(dp[-1])
+print(dp[-1] % MOD)
