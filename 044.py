fd3b693a94688a069555ecf98eb22a1212e48066
Yoshigon <yoshihiro.nakayama.j@gmail.com>
2022-08-30 13:37:47 +0900

    2022/08/30

--- 044.py
+++ 044.py
@@ -1,19 +1,16 @@
 N, Q = map(int, input().split())
 A = list(map(int, input().split()))
 shift = 0
-
 for _ in range(Q):
     t, x, y = map(int, input().split())
-    x += N - (shift+1)
-    y += N - (shift+1)
-    x %= N
-    y %= N
-
     if t == 1:
-        tmp = A[x]
-        A[x] = A[y]
-        A[y] = tmp
+        x_back = (x - shift) % N
+        y_back = (y - shift) % N
+        tmp = A[x_back-1]
+        A[x_back-1] = A[y_back-1]
+        A[y_back-1] = tmp
     elif t == 2:
         shift += 1
     elif t == 3:
-        print(A[x])
+        x_back = (x - shift) % N
+        print(A[x_back-1])
